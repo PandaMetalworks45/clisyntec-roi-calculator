331d2bd44339fd85c83ccb784449054e174b0e5e
PandaMetalworks45 <spencerengelby@gmail.com>
2026-02-02 16:13:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly as go
 
 # --- BRANDING & SETUP ---
 st.set_page_config(page_title="CLISYNTEC Master ROI Calculator", layout="wide")
@@ -7,13 +8,14 @@ st.set_page_config(page_title="CLISYNTEC Master ROI Calculator", layout="wide")
 # --- Animation CSS ---
 st.markdown("""
 <style> 
-.stApp{animation: fadeIn 0.5s ease-in;}
+.stApp{animation: fadeIn 1.0s ease-in;}
 @keyFrames fadeIn {
 0%{opacity: 0; transform: translateY(10px);}
 100%{opacity: 1; transform: translateY(0);}
 }
 </style>
 """, unsafe_allow_html=True)
+
 # Initialize Session State to track which page the user is on
 if 'page' not in st.session_state:
     st.session_state.page = 'menu'
